21d201e5abcc65372561edffcdadbd57683b21d4
oohashi itsuki <c0a25206a0@edu.teu.ac.jp>
2026-04-28 18:02:11 +0900

    空白を入れて #1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -29,7 +29,6 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
 
 
 class Bird:
-
     delta = {  # ここは def の外側！
         pg.K_UP: (0, -5),
         pg.K_DOWN: (0, +5),
@@ -51,11 +50,13 @@ class Bird:
         (+5, +5): pg.transform.rotozoom(img, -45, 0.9), # 右下
     } 
 
+
     def __init__(self, xy: tuple[int, int]):
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
         self.dire = (+5, 0)  # インスタンス変数として向きを保持
+
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -102,10 +103,12 @@ class Beam:
         self.rct.centerx = bird.rct.centerx + bird.rct.width * self.vx / 5
         self.rct.centery = bird.rct.centery + bird.rct.height * self.vy / 5
 
+
     def update(self, screen: pg.Surface):
         # 画面外に出るまで移動
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
+
 
 class Bomb:
     """
@@ -123,6 +126,7 @@ class Bomb:
         self.rct = self.img.get_rect()
         self.rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
         self.vx, self.vy = +5, +5
+
 
     def update(self, screen: pg.Surface):
         """
@@ -168,7 +172,7 @@ def main():
                     screen.blit(txt, [WIDTH//2-150, HEIGHT//2-50])
                     bird.change_img(8, screen)
                     pg.display.update()
-                    time.sleep(1)
+                    # time.sleep(1)
                     return        
 
         # 2. ビームと爆弾の衝突判定（二重ループ）
